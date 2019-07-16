42e481b0cbf01cb3fec4a87d55e8a83aed97e6f9
Ryuta Sakai <info@sakairyuta.com>
2019-07-16 18:47:16 +0900

    Update: Hierarchical post

--- code-to-post.php
+++ code-to-post.php
@@ -4,7 +4,7 @@
  * Plugin URI:      ''
  * Description:     Import static Html to post content
  * Author:          Ryuta Sakai
- * Author URI:      https://github.com/sk-rt
+ * Author URI:      https://github.com/sk-rt/code-to-post
  * Text Domain:     code-to-post
  * Domain Path:     /languages
  * Version:         0.1.0
@@ -31,7 +31,7 @@ class CodeToPost
         return new self();
     }
 
-    public function __construct()
+    private function __construct()
     {
         if (is_admin() && is_user_logged_in()) {
             // メニュー追加
@@ -49,16 +49,15 @@ class CodeToPost
             'Code To Post',
             'Code To Post',
             'manage_options',
-            self::MAIN_MENU_SLUG, /* URL */
-            [$this, 'show_config_form'], /* callback関数 */
-            'dashicons-text', /*  see: https://developer.wordpress.org/resource/dashicons/#awards */
-            99/* 表示位置 */
+            self::MAIN_MENU_SLUG,
+            [$this, 'show_config_form'],
+            'dashicons-text',
+            99
         );
     }
 
     public function show_config_form()
     {
-        // ① wp_optionsのデータをひっぱってくる
         $src_path = get_option(self::DB_PREFIX . self::DB_SLUG_PATH);
 
         ?>
@@ -91,8 +90,9 @@ class CodeToPost
 			<?php do_action(self::PLUGIN_ID . '_veiw_bottom');?>
 
 		</div>
-  <?php
-}
+	<?php
+	}
+
     public function save_config()
     {
         if (isset($_POST[self::CREDENTIAL_SAVE]) && $_POST[self::CREDENTIAL_SAVE]) {
@@ -100,7 +100,7 @@ class CodeToPost
 
                 $path = isset($_POST[self::DB_SLUG_PATH]) ? $_POST[self::DB_SLUG_PATH] : "";
                 update_option(self::DB_PREFIX . self::DB_SLUG_PATH, $path);
-                $completed_text = __("設定の保存が完了しました。", self::TEXT_DOMAIN);
+                $completed_text = __("Update configuration", self::TEXT_DOMAIN);
                 set_transient(self::TRANSIENT_KEY_COMPLETE, $completed_text, 5);
             }
         }
@@ -111,44 +111,34 @@ class CodeToPost
             if (check_admin_referer(self::CREDENTIAL_RUN_ACTION, self::CREDENTIAL_RUN)) {
 
                 $src_path = ABSPATH . get_option(self::DB_PREFIX . self::DB_SLUG_PATH);
-                foreach (glob($src_path . "*") as $filename) {
-                    $post_type = urlencode(self::util_mb_basename($filename));
+                foreach (glob($src_path . "*") as $post_type_dir) {
+                    $post_type = urlencode(self::util_mb_basename($post_type_dir));
                     $post_type_obj = get_post_type_object($post_type);
-                    if (!is_dir($filename) || !$post_type_obj) {
+                    if (!is_dir($post_type_dir) || !$post_type_obj) {
                         continue;
                     }
-                    foreach (glob($filename . '/*.html') as $targetfile) {
-                        $title = esc_html(self::util_mb_basename($targetfile, '.html'));
-                        $slug = sanitize_title($title);
-                        $content = file_get_contents($targetfile);
-                        $post_id;
-                        $is_update = false;
-                        $arg = array(
-                            'name' => $slug,
-                            'post_type' => $post_type,
-                            'posts_per_page' => 1,
-                        );
-                        // var_dump($arg);
-                        $target_posts = get_posts($arg);
-                        if ($target_posts && $target_posts[0]) {
-                            $post_id = wp_update_post(array(
-                                'ID' => $target_posts[0]->ID,
-                                'post_content' => $content,
-                            ));
-                            $is_update = true;
-                        } else {
-                            $post_id = wp_insert_post(array(
-                                'post_title' => $title,
-                                'post_type' => $post_type,
-                                'post_name' => $slug,
-                                'post_content' => $content,
-                                'post_status' => 'publish',
-                            ));
-                        };
-                        if ($post_id) {
-                            add_action(self::PLUGIN_ID . '_veiw_top', function () use ($is_update, $post_id, $post_type_obj) {
-                                self::get_update_message($is_update, $post_id, $post_type_obj);
-                            }, 10, 0);
+                    // 階層構造 あり
+                    if ($post_type_obj->hierarchical) {
+                        $files = self::util_scan_dir($post_type_dir, 'html');
+                        foreach ($files as $file_path) {
+                            $filenameArr = preg_split("/\//", str_replace($post_type_dir . '/', '', $file_path));
+                            $arrCount = count($filenameArr);
+                            if ($arrCount <= 1) {
+                                self::update_post($file_path, $post_type);
+                            } else {
+                                $parent_slug = $filenameArr[$arrCount - 2];
+                                if (
+                                    sanitize_title($parent_slug) !== ''
+                                    && $parent = self::get_posts_by_slug(sanitize_title($parent_slug), $post_type)
+                                ) {
+                                    self::update_post($file_path, $post_type, $parent[0]->ID);
+                                }
+                            }
+                        }
+                        // 階層構造なし
+                    } else {
+                        foreach (glob($post_type_dir . '/*.html') as $file_path) {
+                            self::update_post($file_path, $post_type);
                         }
                     }
 
@@ -156,6 +146,49 @@ class CodeToPost
 
             }
         }
+    }
+
+    private function get_posts_by_slug($slug, $post_type)
+    {
+        $arg = array(
+            'name' => $slug,
+            'post_type' => $post_type,
+            'posts_per_page' => 1,
+        );
+        return get_posts($arg);
+    }
+    private function update_post($filename, $post_type, $post_parent = 0)
+    {
+        $title = esc_html(self::util_mb_basename($filename, '.html'));
+        $slug = sanitize_title($title);
+        $content = file_get_contents($filename);
+        $post_id;
+        $is_update = false;
+
+        $target_posts = self::get_posts_by_slug($slug, $post_type);
+        if ($target_posts && $target_posts[0]) {
+            $post_id = wp_update_post(array(
+                'ID' => $target_posts[0]->ID,
+                'post_content' => $content,
+                'post_parent' => $post_parent,
+            ));
+            $is_update = true;
+        } else {
+            $post_id = wp_insert_post(array(
+                'post_title' => $title,
+                'post_type' => $post_type,
+                'post_name' => $slug,
+                'post_content' => $content,
+                'post_status' => 'publish',
+                'post_parent' => $post_parent,
+            ));
+        };
+        if ($post_id) {
+            add_action(self::PLUGIN_ID . '_veiw_top', function () use ($is_update, $post_id, $post_type) {
+                self::show_update_post_message($is_update, $post_id, $post_type);
+            }, 10, 0);
+        }
+        return $post_id;
     }
     public function show_save_config_message()
     {
@@ -165,27 +198,46 @@ class CodeToPost
 				<p><?=$complete_message?></p>
 			</div>
 		<?php
-};
+		};
     }
-    public function get_update_message($is_update, $post_id, $post_type_obj)
+    public function show_update_post_message($is_update, $post_id, $post_type)
     {
         $label = $is_update ? __("Updated", self::TEXT_DOMAIN) : __("New Posted", self::TEXT_DOMAIN);
         $target_post = get_post($post_id);
+        $post_type_obj = get_post_type_object($post_type);
         ?>
 		<div class="updated">
 			<h3 style="margin-bottom:0.2em"><?=$label . ': "' . esc_html($target_post->post_title)?>"</h3>
 			<p>
-				<span style="background:#999;color:#fff;border-radius:4px;padding:2px 8px;"><?php echo esc_html($post_type_obj->labels->name); ?></span>
+				<span style="background:#999;color:#fff;border-radius:4px;padding:2px 8px;">
+				<?php if ($post_type_obj) {echo esc_html($post_type_obj->labels->name);}?>
+				</span>
 			</p>
 			<p>
-				<strong>ID: </strong><span><?=$target_post->ID?></span>,
+				<strong>ID: </strong><span><?=$target_post->ID?></span>
 				<strong>Slug: </strong><span><?=esc_html($target_post->post_name)?></span><br>
+				<?php if ($target_post->post_parent): ?>
+				<strong>Parent: </strong><span><?=$target_post->post_parent?></span><br>
+				<?php endif;?>
 				<a href="<?=get_edit_post_link($target_post->ID)?>"><?php _e("Edit")?></a> | <a href="<?=get_permalink($target_post->ID)?>"><?php _e("View")?></a>
 			</p>
 		</div>
 		<?php
 }
-    public function util_mb_basename($str, $suffix = null)
+    private function util_scan_dir($dir, $extension = "html")
+    {
+        $list = $tmp = array();
+        foreach (glob($dir . '/*', GLOB_ONLYDIR) as $child) {
+            if ($tmp = self::util_scan_dir($child)) {
+                $list = array_merge($list, $tmp);
+            }
+        }
+        foreach (glob($dir . '/*.' . $extension) as $file) {
+            $list[] = $file;
+        }
+        return $list;
+    }
+    private function util_mb_basename($str, $suffix = null)
     {
         $tmp = preg_split('/[\/\\\\]/', $str);
         $res = end($tmp);
